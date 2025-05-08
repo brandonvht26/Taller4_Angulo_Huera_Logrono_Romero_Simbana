486c8f51225827964f28c40d6c7d72e2c5588f44
POO <bryan.angulo01@epn.edu.ec>
2025-05-08 09:36:45 -0500

    Angulo scanner

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         // 4. Control de Velocidad de un Automóvil
         //   Crea un sistema para controlar la velocidad de un automóvil, asegurándote de que no se excedan los límites establecidos y que la velocidad nunca sea negativa.4. Control de Velocidad de un Automóvil
-        //   Crea un sistema para controlar la velocidad de un automóvil, asegurándote de que no se excedan los límites establecidos y que la velocidad nunca sea negativa.
+
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese la velocidad");
-
         float velocidad = sc.nextFloat();
-
+        System.out.println("Ingrese la marca");
+        float modelo= sc.nextFloat();
 
 
     }
